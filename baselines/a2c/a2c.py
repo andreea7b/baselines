d7f4341d355a88d7ab910e353e3df76740bf8302
Andreea Bobu <abobu@berkeley.edu>
2018-01-27 15:16:27 -0800

    some progress on distillation; introduced subsampling, replay buffers, but not sure how to do the actual loss

--- baselines/a2c/a2c.py
+++ baselines/a2c/a2c.py
@@ -21,11 +21,18 @@ class Model(object):
 
     def __init__(self, policy, ob_space, ac_space, nenvs, nsteps,
             ent_coef=0.01, vf_coef=0.5, max_grad_norm=0.5, lr=7e-4,
-            alpha=0.99, epsilon=1e-5, total_timesteps=int(80e6), lrschedule='linear'):
+            alpha=0.99, epsilon=1e-5, total_timesteps=int(80e6),
+                 lrschedule='linear', replay_lambda=1, ss_rate=1,
+                 replay_loss=None):
 
         sess = tf_util.make_session()
         nact = ac_space.n
         nbatch = nenvs*nsteps
+
+        # If we have replay_loss, create replay buffer and stage buffer
+        # Use this to enforce replay loss lower
+        if replay_loss is not None:
+            self.replay_buffer = [] # holds all past data
 
         A = tf.placeholder(tf.int32, [nbatch])
         ADV = tf.placeholder(tf.float32, [nbatch])
@@ -39,8 +46,21 @@ class Model(object):
         pg_loss = tf.reduce_mean(ADV * neglogpac)
         vf_loss = tf.reduce_mean(mse(tf.squeeze(train_model.vf), R))
         entropy = tf.reduce_mean(cat_entropy(train_model.pi))
-        loss = pg_loss - entropy*ent_coef + vf_loss * vf_coef
 
+        # Introduce replay_loss if given
+        if replay_loss == "L2":
+            # Replace train_model.pi with whatever is predicted label
+            # Replace A with whatever is recorded label
+            re_loss = tf.nn.l2_loss(tf.nn.softmax(train_model.pi) - A) / nbatch
+        elif replay_loss == "Distillation":
+            # Replace y_donor with whatever is recorded label
+            # Replace y_acceptor with whatever is predicted label
+            re_loss = tf.reduce_mean( - tf.reduce_sum(tf.stop_gradient(y_donor)
+                                                      * tf.log(y_acceptor),
+                                                      reduction_indices=1))
+        loss = pg_loss - entropy*ent_coef + vf_loss * vf_coef
+        if replay_loss is not None:
+            loss = loss + replay_lambda*re_loss
         params = find_trainable_variables("model")
         grads = tf.gradients(loss, params)
         if max_grad_norm is not None:
@@ -89,15 +109,18 @@ class Model(object):
 
 class Runner(object):
 
-    def __init__(self, env, model, nsteps=5, gamma=0.99):
+    def __init__(self, env, model, nsteps=5, gamma=0.99, theta=0):
         self.env = env
         self.model = model
+        self.replay_buffer = []
         nh, nw, nc = env.observation_space.shape
         nenv = env.num_envs
         self.batch_ob_shape = (nenv*nsteps, nh, nw, nc)
         self.obs = np.zeros((nenv, nh, nw, nc), dtype=np.uint8)
         self.nc = nc
+        self.theta = theta
         obs = env.reset()
+        obs = self.invert_frame(obs)
         self.gamma = gamma
         self.nsteps = nsteps
         self.states = model.initial_state
@@ -108,11 +131,14 @@ class Runner(object):
         mb_states = self.states
         for n in range(self.nsteps):
             actions, values, states, _ = self.model.step(self.obs, self.states, self.dones)
+            # Save the result of this transition in the buffer
+            replay_buffer.append((np.copy(self.obs), self.states, self.dones, actions))
             mb_obs.append(np.copy(self.obs))
             mb_actions.append(actions)
             mb_values.append(values)
             mb_dones.append(self.dones)
             obs, rewards, dones, _ = self.env.step(actions)
+            obs = self.invert_frame(obs)
             self.states = states
             self.dones = dones
             for n, done in enumerate(dones):
@@ -145,15 +171,24 @@ class Runner(object):
         mb_masks = mb_masks.flatten()
         return mb_obs, mb_states, mb_rewards, mb_masks, mb_actions, mb_values
 
-def learn(policy, env, seed, nsteps=5, total_timesteps=int(80e6), vf_coef=0.5, ent_coef=0.01, max_grad_norm=0.5, lr=7e-4, lrschedule='linear', epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100):
+    def invert_frame(self, frame):
+        return (1 - self.theta)*frame + self.theta*(1 - frame)
+
+def learn(policy, env, seed, nsteps=5, total_timesteps=int(80e6), vf_coef=0.5,
+          ent_coef=0.01, max_grad_norm=0.5, lr=7e-4, lrschedule='linear',
+          epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100):
     tf.reset_default_graph()
     set_global_seeds(seed)
 
     nenvs = env.num_envs
     ob_space = env.observation_space
     ac_space = env.action_space
-    model = Model(policy=policy, ob_space=ob_space, ac_space=ac_space, nenvs=nenvs, nsteps=nsteps, ent_coef=ent_coef, vf_coef=vf_coef,
-        max_grad_norm=max_grad_norm, lr=lr, alpha=alpha, epsilon=epsilon, total_timesteps=total_timesteps, lrschedule=lrschedule)
+    model = Model(policy=policy, ob_space=ob_space, ac_space=ac_space,
+                  nenvs=nenvs, nsteps=nsteps, ent_coef=ent_coef,
+                  vf_coef=vf_coef, max_grad_norm=max_grad_norm, lr=lr, alpha=alpha,
+                  epsilon=epsilon, total_timesteps=total_timesteps,
+                  lrschedule=lrschedule, replay_lambda=replay_lambda,
+                  ss_rate=ss_rate, replay_loss=replay_loss)
     runner = Runner(env, model, nsteps=nsteps, gamma=gamma)
 
     nbatch = nenvs*nsteps
@@ -173,3 +208,47 @@ def learn(policy, env, seed, nsteps=5, total_timesteps=int(80e6), vf_coef=0.5, e
             logger.record_tabular("explained_variance", float(ev))
             logger.dump_tabular()
     env.close()
+
+def learn_staged(policy, env, seed, nsteps=5, total_timesteps=int(80e6), vf_coef=0.5,
+          ent_coef=0.01, max_grad_norm=0.5, lr=7e-4, lrschedule='linear',
+          epsilon=1e-5, alpha=0.99, gamma=0.99, log_interval=100,
+          replay_lambda=1, ss_rate=1, replay_loss=None, thetas=None):
+    tf.reset_default_graph()
+    set_global_seeds(seed)
+
+    nenvs = env.num_envs
+    ob_space = env.observation_space
+    ac_space = env.action_space
+    model = Model(policy=policy, ob_space=ob_space, ac_space=ac_space,
+                  nenvs=nenvs, nsteps=nsteps, ent_coef=ent_coef,
+                  vf_coef=vf_coef, max_grad_norm=max_grad_norm, lr=lr, alpha=alpha,
+                  epsilon=epsilon, total_timesteps=total_timesteps,
+                  lrschedule=lrschedule, replay_lambda=replay_lambda,
+                  ss_rate=ss_rate, replay_loss=replay_loss)
+
+    nbatch = nenvs*nsteps
+    tstart = time.time()
+    for theta in thetas:
+        # Reset the runner
+        runner = Runner(env, model, nsteps=nsteps, gamma=gamma, theta=theta)
+        for update in range(1, total_timesteps//nbatch+1):
+            obs, states, rewards, masks, actions, values = runner.run()
+            policy_loss, value_loss, policy_entropy = model.train(obs, states, rewards, masks, actions, values)
+            nseconds = time.time()-tstart
+            fps = int((update*nbatch)/nseconds)
+            if update % log_interval == 0 or update == 1:
+                ev = explained_variance(values, rewards)
+                logger.record_tabular("nupdates", update)
+                logger.record_tabular("total_timesteps", update*nbatch)
+                logger.record_tabular("fps", fps)
+                logger.record_tabular("policy_entropy", float(policy_entropy))
+                logger.record_tabular("value_loss", float(value_loss))
+                logger.record_tabular("explained_variance", float(ev))
+                logger.dump_tabular()
+        # We're done with the stage, so sample the replay buffer:
+        num_samples = int(ss_rate * len(runner.replay_buffer))
+        past_data_idx = random.sample(range(0, len(runner.replay_buffer)), num_samples)
+        past_data = runner.replay_buffer[past_data_idx]
+        model.replay_buffer.extend(past_data)
+    env.close()
+
--- baselines/a2c/run_atari.py
+++ baselines/a2c/run_atari.py
@@ -6,7 +6,8 @@ from baselines.common.vec_env.vec_frame_stack import VecFrameStack
 from baselines.a2c.a2c import learn
 from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
 
-def train(env_id, num_timesteps, seed, policy, lrschedule, num_env):
+def train(env_id, num_timesteps, seed, policy, lrschedule, num_env,
+          replay_lambda=1, replay_loss=None, ss_rate=1, thetas=None):
     if policy == 'cnn':
         policy_fn = CnnPolicy
     elif policy == 'lstm':
@@ -14,17 +15,28 @@ def train(env_id, num_timesteps, seed, policy, lrschedule, num_env):
     elif policy == 'lnlstm':
         policy_fn = LnLstmPolicy
     env = VecFrameStack(make_atari_env(env_id, num_env, seed), 4)
-    learn(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule)
+    if replay_loss is not None:
+        learn_staged(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1),
+          lrschedule=lrschedule, replay_lambda=replay_lambda, ss_rate=ss_rate,
+         replay_loss=replay_loss, thetas=thetas)
+    else:
+        learn(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule)
     env.close()
 
 def main():
     parser = atari_arg_parser()
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--lrschedule', help='Learning rate schedule', choices=['constant', 'linear'], default='constant')
+    parser.add_argument('--replay_lambda', help='Replay regularizer parameter', default=1)
+    parser.add_argument('--ss_rate', help='Subsampling rate', default=1)
+    parser.add_argument('--replay_loss', help='Replay loss, if any', choices=['L2', 'Distillation'], default=None)
+    parser.add_argument('--thetas', help='List of thetas to invert over', nargs='*', default=None)
     args = parser.parse_args()
     logger.configure()
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
-        policy=args.policy, lrschedule=args.lrschedule, num_env=16)
+        policy=args.policy, lrschedule=args.lrschedule, num_env=16,
+          replay_lambda=args.replay_lambda, ss_rate=args.ss_rate,
+          replay_loss=args.replay_loss, thetas=args.thetas)
 
 if __name__ == '__main__':
     main()
